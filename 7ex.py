0b3862d7ee67faf399d7d7173d0de47b0da43738
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2022-06-16 16:33:31 -0300

    Pensando na lógica

---
+++ 7ex.py
@@ -0,0 +1,29 @@
+"""
+
+7. Imagine que em uma barraca de cachorro quente, cada cachorro quente
+custe R$4,00.Um copo de refrigerante custa R$ 2.00. A cada três cachorros
+quentes, ganha-se gratuitamente 1 refrigerante ou R$1.00 de desconto.
+Escreva um programa e um DFD que solicite ao usuário a quantidade de
+cachorros quentes, a quantidade de refrigerantes e diga ao usuário qual
+deve ser o total a pagar, considerando os descontos dos refrigerantes
+gratuitos. 
+
+Exemplos:
+
+------------------------------------------------------------------------
+Cachorros Q.| Refrigerantes G. | Refri Pedidos| Desc. Rest. |   T.P.   | 
+    10      |        3         |      3       |     0       | R$ 40,00 |
+    3       |        1         |      2       |     0       | R$ 14,00 |
+    7       |        2         |      1       |     1       | R$ 27,00 |
+------------------------------------------------------------------------
+
+
+"""
+
+
+# Cada cachorro quente custa 4 reais
+
+# Cada copo custa 2 reais
+
+# Cada 3 cachorro quente ganha 1 refri ou 1$ real de desconto
+
